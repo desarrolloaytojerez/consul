cd0fceba223f40f1ea908dabcb0928aeddb3a97e
Juan Rebollo <juan.rebollo@aytojerez.es>
2018-10-09 14:43:02 +0200

    Single language es

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,10 @@
 module Consul
   class Application < Rails::Application
+
+    config.i18n.default_locale = :es
+    config.i18n.available_locales = [:es]
+    config.i18n.enforce_available_locales = false
+    config.i18n.fallbacks = nil
+
   end
 end
